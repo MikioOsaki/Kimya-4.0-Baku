59e9bb475a4a72a539d362b416b0a5f952de53ab
gituser <mikio.osaki@posteo.de>
2017-12-15 16:04:26 +0100

    initial 2

---
+++ app/src/main/java/com/example/mikio/kimya_3_0_Yevlakh/DataFish.java
@@ -0,0 +1,18 @@
+package com.example.mikio.kimya_3_0_Yevlakh;
+
+public class DataFish {
+
+    public final String fishName;
+    public final String catName;
+    public final String sizeName;
+    public final int price;
+    public final String url;
+
+    public DataFish(String fishName, String catName, String sizeName, int price, String url) {
+        this.fishName = fishName;
+        this.catName = catName;
+        this.sizeName = sizeName;
+        this.price = price;
+        this.url = url;
+    }
+}
