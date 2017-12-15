8334fce073defa9698860cb299c0a02ba22bc710
gituser <mikio.osaki@posteo.de>
2017-12-15 16:28:29 +0100

    php file changed, new sql statement

--- PHP/fish-search.php
+++ PHP/fish-search.php
@@ -4,7 +4,10 @@
      {
      	  require_once('config.inc.php');
 	  $search_query='%' . $_POST['searchQuery'] . '%';
-          $sql = 'SELECT * FROM tbl_fish WHERE fish_name LIKE :search_query';
+          $sql = 'SELECT * FROM tbl_fish 
+          WHERE fish_name LIKE :search_query
+          OR cat_name LIKE :search_query
+          ';
           $statement = $connection->prepare($sql);
 	  $statement->bindParam(':search_query', $search_query, PDO::PARAM_STR);
           $statement->execute();
